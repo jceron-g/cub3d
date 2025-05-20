403bfc74fdc943b65aafbc990dfc1d92cd6b749f
gitmario3d <140595777+gitmario3d@users.noreply.github.com>
2025-05-20 17:20:05 +0200

    player_movement.c

---
+++ src/player_movement.c
@@ -0,0 +1,66 @@
+#include "../include/cub3d.h"
+
+int	move_forward(t_cube *cube)
+{
+  t_player  *player;
+	double	new_x;
+	double	new_y;
+
+  player = cube->player;
+	new_x = player.pos_x + player.dir_x * MOVE_SPEED;
+	new_y = player.pos_y + player.dir_y * MOVE_SPEED;
+	return (0);
+}
+
+int	move_backward(t_cube *cube)
+{
+  t_player  *player;
+	double	new_x;
+	double	new_y;
+
+  player = cube->player;
+	new_x = player.pos_x - player.dir_x * MOVE_SPEED;
+	new_y = player.pos_y - player.dir_y * MOVE_SPEED;
+	return (0);
+}
+
+int	move_left(t_cube *cube)
+{
+  t_player  *player;
+	double	new_x;
+	double	new_y;
+
+  player = cube->player;
+	new_x = player.pos_x + player.dir_y * MOVE_SPEED;
+	new_y = player.pos_y - player.dir_x * MOVE_SPEED;
+	return (0);
+}
+
+int	move_right(t_cube *cube)
+{
+  t_player  *player;
+	double	new_x;
+	double	new_y;
+
+  player = cube->player;
+	new_x = player.pos_x - player.dir_y * MOVESPEED;
+	new_y = player.pos_y + player.dir_x * MOVESPEED;
+	return (0);
+}
+
+void	player_movement(t_cube *cube)
+{
+  t_player  *player;
+
+  player = cube->player;
+	if (player.move_y == 1)
+		move_forward(cube);
+	if (player.move_y == -1)
+		move_backward(cube);
+	if (player.move_x == -1)
+		move_left(cube);
+	if (player.move_x == 1)
+		move_right(cube);
+	if (player.rotate != 0)
+		rotate_player(cube, player.rotate);
+}
